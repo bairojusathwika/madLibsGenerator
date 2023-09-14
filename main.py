597cecd00ff3fb2874624415346d03b872470689
bairojusathwika <102368330+bairojusathwika@users.noreply.github.com>
2023-09-14 13:33:00 +0530

    Add files via upload

---
+++ main.py
@@ -0,0 +1,13 @@
+
+a = input("Your Last Name: ")
+b = input("Vacation Destination: ")
+c = input("One item taken to vacation: ")
+d = input("Another item taken to vacation: ")
+x = input("Unusual Item: ")
+y = input("One of your family member's name:")
+z = input("Number: ")
+o = input("Verb: ") #fun activity
+
+madlibs = f"Once upon a time, the {a} family decided to go on a vacation to {b}.Everyone was excited,especially {y}, who couldn't wait to {o}.On the first day, the family packed their bags with {c}, {d}, and even {x}.During the car ride, {y} kept asking ""Are we there yet?"" every {z} minutes, which drove everyone crazy."
+
+print(madlibs)
